47099cdc2c4d968d500341e0ade89457626ee919
Сергій Нумержицький <rist1cnum@gmail.com>
2022-01-17 12:54:59 +0200

    cloud API key in secrets

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,7 @@
 class ContactController < ApplicationController
   def index
+
+    @url_maps = 'https://maps.googleapis.com/maps/api/js?key='+Rails.application.credentials.google_cloude[:key]+'&callback=initMap&libraries=&v=weekly&channel=2'
+    
   end
 end
